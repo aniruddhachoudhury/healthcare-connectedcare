2741473406e2e89af32d91c398664bb37bf65dc6
shaunakc11 <helloshaunak276@gmail.com>
2025-10-24 18:49:53 +0530

    rename jsx to js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
+import { CTA } from "@/components/cta";
+import { Features } from "@/components/features";
+import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import { Hero } from "@/components/hero";
-import { Features } from "@/components/features";
+import { Security } from "@/components/security";
 import { Services } from "@/components/services";
 import { Specialties } from "@/components/specialties";
 import { Stats } from "@/components/stats";
-import { CTA } from "@/components/cta";
-import { Footer } from "@/components/footer";
-import { Security } from "@/components/security";
 
 export default function Home() {
   return (
