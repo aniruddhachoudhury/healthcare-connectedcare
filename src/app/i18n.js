a0bcfe762107755e3dea946799f6c333dc1bbdd3
Abhijit Patra <apatra@juniper.net>
2025-10-24 13:47:06 +0530

    Adding the intial layout

---
+++ src/app/i18n.js
@@ -0,0 +1,18 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import en from "..\\locale\\en.json";
+import hi from "..\\locale\\hi.json";
+import bn from "..\\locale\\bn.json";
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: { en: { translation: en }, hi: { translation: hi }, bn: { translation: bn } },
+    fallbackLng: "en",
+    interpolation: { escapeValue: false }
+  });
+
+export default i18n;
